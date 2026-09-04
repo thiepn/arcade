9586dede160bb1e77589340cfb5d2a31423ced59
thiepn <junsosmart@gmail.com>
2026-09-04 15:50:42 +0200

    Install P18 clarity runtime

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { installGameFeelRuntime } from './lib/gameFeelRuntime';
+import { installGameClarityRuntime } from './lib/gameClarityRuntime';
 import { installMobileRuntimeCompatibility } from './lib/mobileRuntime';
 import './index.css';
 import './p17-game-feel.css';
+import './p18-clarity-accessibility.css';
 
 installMobileRuntimeCompatibility();
 installGameFeelRuntime();
+installGameClarityRuntime();
 
 window.addEventListener('vite:preloadError', (event) => {
   event.preventDefault();
